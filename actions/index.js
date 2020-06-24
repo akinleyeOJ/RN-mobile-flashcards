7355cb22aee70a620ab51ae61cb1d3bde3ecc5df
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-24 21:12:03 +0100

    chore: define actions and reducer files

---
+++ actions/index.js
@@ -0,0 +1,24 @@
+export const ADD_DECK = 'ADD_DECK'
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK'
+export const RECIEVE_DECKS = 'RECIEVE_DECKS'
+
+export function addDeck (deck) {
+    return {
+        type: ADD_DECK,
+        deck
+    }
+}
+
+export function addCard (card) {
+    return {
+        type: ADD_CARD_TO_DECK,
+        card
+    }
+}
+
+export function recieveDeck (decks) {
+    return {
+        type: RECIEVE_DECKS,
+        decks
+    }
+}
