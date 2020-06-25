00094e7c32c6eee8dd6771c996d03302aaad90da
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-26 00:34:03 +0100

    chore: add progress to newDeck component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, TextInput, Button, View} from "react-native";
+import {saveDeckTitle} from '../utils/api'
+import {AddDeck, addDeck} from "../actions"
+import {connect} from "react-redux"
+
+import SubmitButton from "./SubmitButton";
+
 
 export class NewDeck extends Component {
+    state = {
+        text: ''
+    }
+     submitName=() => {
+         const { text } = this.state
+     
+   saveDeckTitle(text)
+   this.props.dispatch(addDeck(text))
+   this.props.navigation.navigate("ViewDeck", {entryId: text })
+   this.setState({ text: ''})
+}
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>1</Text>
+                <Text style={styles.title}>New Decks Title?</Text>
+                <TextInput style={styles.input} onChangeText={(text) => this.setState({ text: text})} value={this.state.text}>
+                </TextInput>
+                <SubmitButton
+                   style={styles.submitBtn} onPress={(this.submitName)}
+                 />
             </View>
         )
     }
@@ -15,10 +38,28 @@ export class NewDeck extends Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    title: {
+        fontSize: 30,
+        color: "#333"
+    },
+    submitBtn: {
+        borderWidth: 0.5,
+        borderColor: "#d6d7da",
+        padding: 10,
+        borderRadius: 7
+    },
+    input: {
+        width: 200,
+        height: 44,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: "#757575",
+        margin: 50,
+        borderRadius: 8
+    }
   });
 
-export default NewDeck
+export default connect()(NewDeck)
